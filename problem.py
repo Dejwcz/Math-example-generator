3f182427ce24cc203eadea93e8d35ff5c0a5e624
David Smětala <x213@centrum.cz>
2023-12-01 23:08:17 +0100

    class Problem seems to be good for now

--- problem.py
+++ problem.py
@@ -1,9 +1,8 @@
 from random import choice
 from random import randint
 
-class problem():
+class Problem():
     
-
     def __init__(self, numcount = 2, operator = [" + "," - "],
                  interval = [-1000,1000], decimal = 0):
         self.numcount = numcount
@@ -13,23 +12,8 @@ class problem():
         self.numbers = []
         self.operators = []
         self.generate_problem()
-        
-# def __str__(self) -> str:
-#     numbers = []
-#     number = int
-#     problemstr = ""
-#     for i in range(self.numcount):
-#         modifier = 10**self.decimal
-#         number = randint(self.interval[0]*modifier,
-#                          self.interval[1]*modifier)/modifier
-#         # modifier is for getting decimal numbers
-#         numbers.append(str(number) if number>=0 else "("+str(number)+")") 
-#     for j in numbers:
-#         operator = choice(self.operator)
-#         problemstr = problemstr + str(j) + operator
-#         self.problemx = problemstr[:-3] + " = " #[:-3]-because last redundant operator
-#     return self.problemx
-    
+        self.result = self.solve_it()
+       
     def generate_problem(self):
         modifier = 10**self.decimal
         for i in range(self.numcount):
@@ -39,13 +23,48 @@ class problem():
             self.operators.append(choice(self.operator))
 
     def __str__(self) -> str:
-        return (str(self.numbers) + str(self.operators))
+        problemstr = ""
+        for i in range(len(self.operators)):
+            problemstr = problemstr + \
+                (str(self.numbers[i]) if self.numbers[i] >= 0
+                else "("+str(self.numbers[i])+")")+self.operators[i]
+        problemstr = problemstr+\
+            (str(self.numbers[-1]) if self.numbers[-1] >= 0
+            else "("+str(self.numbers[-1])+")")+ " = "
+        return problemstr
+    
+    def __len__(self):
+        return len(self.__str__())
+    
+    def solve_it(self):
+        help_result = self.numbers[0]
+        for i in range(len(self.numbers)-1):
+            if self.operators[i] == " + ":
+                help_result += self.numbers[i+1]
+            elif self.operators[i] == " - ":
+                help_result -= self.numbers[i+1]
+            elif self.operators[i] == " * ":
+                help_result *= self.numbers[i+1]
+            elif self.operators[i] == " / ":
+                help_result /= self.numbers[i+1]
+        return round(help_result,self.decimal)
+    
+    def whole_example(self):
+        return self.__str__() + str(self.solve_it())
+
+
+
+
         
-p = problem(5,[" / ", " * "],interval=[0,50],decimal=3)
-d = problem(2)
-print(p)
-print(p)
-print(d)
-print(d)
+#p = Problem(5,[" / ", " * "," + "],interval=[-50,50],decimal=3)
+#d = Problem(2, [" + "])
+#print(p)
+#print(p.solve_it())
+#print(p.whole_example())
+#print(d)
+#print(d.solve_it())
+#print(len(d))
+
+
 
 
